b96a45acc597c94d294305435564a1f7e848a5f6
namo02268 <namo02268@gmail.com>
2022-10-20 17:09:34 +0900

    [Add] Rotating Box

--- js/GL/Buffer.js
+++ js/GL/Buffer.js
@@ -1,8 +1,8 @@
-class Buffer {
+class VertexBuffer {
   // private member variables
   #m_id;
 
-  constructor() {
+  constructor(type) {
     this.#m_id = gl.createBuffer();
   }
 
@@ -13,6 +13,28 @@ class Buffer {
   SetData(data) {
     this.Bind();
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+  }
+
+  GetID() {
+    return this.#m_id;
+  }
+}
+
+class IndexBuffer {
+  // private member variables
+  #m_id;
+
+  constructor(type) {
+    this.#m_id = gl.createBuffer();
+  }
+
+  Bind() {
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.#m_id);
+  }
+
+  SetData(data) {
+    this.Bind();
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data), gl.STATIC_DRAW);
   }
 
   GetID() {
--- js/GL/Scene.js
+++ js/GL/Scene.js
@@ -1,25 +1,13 @@
-let squareRotation = 0.0;
+let cubeRotation = 0.0;
 
 function drawScene(deltaTime) {
   //-------------------Buffer-------------------//
-  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
-  const positionBuffer = new Buffer(gl);
-  positionBuffer.SetData(positions);
-  const colors = [
-    1.0, 1.0, 1.0, 1.0,
-    1.0, 0.0, 0.0, 1.0,
-    0.0, 1.0, 0.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-  ];
-  const colorBuffer = new Buffer(gl);
-  colorBuffer.SetData(colors);
-
-  //-------------------Window-------------------//
-  gl.clearColor(0.2, 0.2, 0.2, 1.0); // Clear to black, fully opaque
-  gl.clearDepth(1.0); // Clear everything
-  gl.enable(gl.DEPTH_TEST); // Enable depth testing
-  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  const positionBuffer = new VertexBuffer();
+  positionBuffer.SetData(Box.positions);
+  const colorBuffer = new VertexBuffer();
+  colorBuffer.SetData(Box.colors);
+  const indexBuffer = new IndexBuffer();
+  indexBuffer.SetData(Box.indices);
 
   //-------------------Shader-------------------//
   const vsSource = `
@@ -45,6 +33,13 @@ function drawScene(deltaTime) {
   `;
   const shader = new Shader(vsSource, fsSource);
 
+  //-------------------Window-------------------//
+  gl.clearColor(0.2, 0.2, 0.2, 1.0); // Clear to black, fully opaque
+  gl.clearDepth(1.0); // Clear everything
+  gl.enable(gl.DEPTH_TEST); // Enable depth testing
+  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
   //-------------------Camera-------------------//
   const fieldOfView = (45 * Math.PI) / 180; // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
@@ -53,14 +48,17 @@ function drawScene(deltaTime) {
 
   const projectionMatrix = mat4.create();
   mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
   const modelViewMatrix = mat4.create();
   mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
-  squareRotation += deltaTime;
-  mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation, [0, 0, 1]);
+  cubeRotation += deltaTime;
+  mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 0, 1]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * 0.7, [0, 1, 0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * 0.3, [1, 0, 0]);
 
   //-------------------Model-------------------//
   {
-    const numComponents = 2
+    const numComponents = 3
     const type = gl.FLOAT;
     const normalize = false;
     const stride = 0;
@@ -101,8 +99,10 @@ function drawScene(deltaTime) {
   shader.SetUniformMatrix4("uModelViewMatrix", modelViewMatrix);
 
   {
+    const vertexCount = 36;
+    const type = gl.UNSIGNED_SHORT;
     const offset = 0;
-    const vertexCount = 4;
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    indexBuffer.Bind();
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
   }
 }
